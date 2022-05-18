ea11289b34fb5fa9e480b5f087cbf8d7db254b9f
Young-yun Jeong <yy.jeong@crowdworks.kr>
2022-05-18 15:30:04 +0900

    add userAsync.js

--- src/Users.js
+++ src/Users.js
@@ -1,51 +1,15 @@
-import {useEffect, useReducer} from "react";
 import axios from "axios";
+import useAsync from "./useAsync";
 
-function reducer(state, action) {
-    switch (action.type) {
-        case 'LOADING':
-            return {
-                loading: true,
-                data: null,
-                error: null
-            };
-        case 'SUCCESS':
-            return {
-                loading: false,
-                data: action.data,
-                error: null
-            };
-        case 'ERROR':
-            return {
-                loading: false,
-                data: null,
-                error: action.error
-            };
-        default:
-            throw new Error(`Unhandled action type: ${action.type}`);
-    }
+async function getUsers() {
+    const response = await axios.get(
+        'https://jsonplaceholder.typicode.com/users'
+    );
+    return response.data;
 }
 
 function Users() {
-    const [state, dispatch] = useReducer(reducer, {
-        loading: false,
-        data: null,
-        error: null
-    });
-
-    const fetchUsers = async () => {
-        dispatch({type: 'LOADING'});
-        try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-            dispatch({type: 'SUCCESS', data: response.data});
-        } catch (e) {
-            dispatch({type: 'ERROR', error: e});
-        }
-    };
-
-    useEffect(() => {
-        fetchUsers();
-    }, []);
+    const [state, fetchData] = useAsync(getUsers, []);
 
     const {loading, data: users, error} = state;
 
@@ -61,7 +25,7 @@ function Users() {
                     </li>
                 ))}
             </ul>
-            <button onClick={fetchUsers}>Reload</button>
+            <button onClick={fetchData}>Reload</button>
         </>
     );
 }
